a2a9807c8baa981934681231e169d62201f89632
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 10:07:37 -0700

    add acronymize

--- algorithm.js
+++ algorithm.js
@@ -1,3 +1,36 @@
 import { print_desc } from "./module.js";
 import { print_ln } from "./module.js";
 
+const str1 = " there's no free lunch - gotta pay yer way. ";
+const expected1 = "TNFL-GPYW";
+
+const str2 = "Live from New York, it's Saturday Night!";
+const expected2 = "LFNYISN";
+
+
+// /**
+//  * Turns the given str into an acronym.
+//  * - Time: O(?).
+//  * - Space: O(?).
+//  * @param {string} str A string to be turned into an acronym.
+//  * @returns {string} The given str converted into an acronym.
+//  */
+function acronymize(str) {
+    var acronym = "";
+
+    if (str[0] != " ") {                    // Edge Case
+        acronym += str[0];
+    }
+
+    for (var i=0; i < str.length; i++) {    // loop through the string
+
+        if (str[i] == " ") {
+            acronym += str[i+1];
+            // console.log(acronym);
+        }7
+    }
+    return acronym.toUpperCase();
+
+}
+
+console.log(acronymize ("Live from New York, it's Saturday Night!"));
